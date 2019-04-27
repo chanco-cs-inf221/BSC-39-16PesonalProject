593e7dd3cefecfb75581251a8038ec5035764361
chrispharaoh <48133104+chrispharaoh@users.noreply.github.com>
2019-04-27 02:36:21 +0200

    Add files via upload

---
+++ questions.js
@@ -0,0 +1,15 @@
+var questions = [
+  ["Who Founded Malawi?", "The Tumbukas", "The Ngoni's", "Maravi people","The Akafula", "3"],
+  [" Who was the fist president of Malawi?", "John Chilembwe", "Hastings Kamuzu Banda", " Dr David Livingstone", "Evaton Chimulirenji","2"],
+  ["Where did Chancellor college Firstly located?", "Chichiri Secondary School in Blantyre", "Matawale", "At Ndata farm", "In Balaka","1"],
+  [" How many districts are in malawi?", " 20", " 24", "28", "30","3"],
+  ["Which of the following statements are true, and which are false?", "  Only one of the statements is false ", " Exactly four of the statements are false", "  Only three of the statements are false", "All five of these statements are false","2"],
+  [" Who composed Malawi National Anthem?", "  Dr Saulosi Chilima", "  Allan Namoko", " Michael Fredlick Paul Sauka", "Non of the above","3"],
+  ["What makes bus to be busy?", " y", "  driver", "passengers", "Traffic robbots","1"],
+  ["What is the meaning of CSS?", " Programming language", " Cascading Style Sheets for styling htm document", "Web server", "All of the above","2"],
+  ["Who was the first commisioner of Malawi?", "Harry Hamilton Johnston,", "Robert Perceval Armitage,", "Glyn Smallwood Jones", "Harold Baxter Kittermaster","1"],
+  [" What can travel around the world while staying in a corner?", "hellocopter", "pilote", "stamp", "Phone call","3"],
+  [" If two days before today was tuesday, what is the day two days after tomorrow?", " Tuesday", "Friday", "Wednessday", "Sunday","2"],
+  ["Brothers and sisters I have none but this man's father is my father's son.Who is the man?"
+  , "The man is my son", "  The man is my uncle", " The man is my father", "The man is my brother","1"],
+  ];
